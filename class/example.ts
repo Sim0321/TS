e4bcf5bb5a47acde77ac422558fc8b69eabfd579
Sim <0doogadooga@gmail.com>
2022-12-04 21:55:36 +0900

    readonly properties

--- class/example.ts
+++ class/example.ts
@@ -6,6 +6,12 @@ class Person {
   //   this.name = name;
   // }
   // constructor 에는 async 붙힐 수 없음
+
+  public readonly name: string = "Sim"; // 값을 못바꾸게 하려면 readonly 사용
+  // private readonly country: string = "Korea"; // 외부에서 this.country 접근은 가능하지만 readonly이기 때문에 값을 변경하진 못함(에러 발생)
+  //위에 두개 처럼 프로퍼티 초기화를 선언하는 곳에서도 가능했고
+  private readonly country: string;
+
   public constructor(private _name: string, private age: number) {
     // if (age === undefined) { // 두개의 class 생성을 하고싶으면 ex p1, p2 조건부로 제한
     //   this.age = 20;
@@ -13,17 +19,24 @@ class Person {
     //   this.age = age;
     // }
     // 원래는 this.name = name , this.age = age 이렇게 작성을 해야하지만 코드가 길어지므로 지워버리고 constructor 매개변수 앞에 접근제어자를 붙여주면 name: string; age: number; 이렇게 Person class에서 선언한 것 과 동일한 결과가 나옴
+
+    // constructor 안에서도 프로퍼티 초기화를 할 수 있다.
+    this.country = "Korea";
   }
 
-  get name() {
-    // 함수이므로 무언가 로직을 만들 수 있음
-    // console.log("get");
-    return this._name + "잘생긴";
-  }
+  // get name() {
+  //   // 함수이므로 무언가 로직을 만들 수 있음
+  //   // console.log("get");
+  //   return this._name + "잘생긴";
+  // }
 
-  set name(name1: string) {
-    console.log("set");
-    this._name = name1;
+  // set name(name1: string) {
+  //   console.log("set");
+  //   this._name = name1;
+  // }
+
+  hello() {
+    this.country = "China";
   }
 }
 
