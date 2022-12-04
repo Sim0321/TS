7ba5890378ac1d9f261ab53c46c0d273f6e01556
Sim <0doogadooga@gmail.com>
2022-12-04 21:48:07 +0900

    getter, setter

--- class/example.js
+++ class/example.js
@@ -6,8 +6,8 @@ class Person {
     //   this.name = name;
     // }
     // constructor 에는 async 붙힐 수 없음
-    constructor(name, age) {
-        this.name = name;
+    constructor(_name, age) {
+        this._name = _name;
         this.age = age;
         // if (age === undefined) { // 두개의 class 생성을 하고싶으면 ex p1, p2 조건부로 제한
         //   this.age = 20;
@@ -16,9 +16,20 @@ class Person {
         // }
         // 원래는 this.name = name , this.age = age 이렇게 작성을 해야하지만 코드가 길어지므로 지워버리고 constructor 매개변수 앞에 접근제어자를 붙여주면 name: string; age: number; 이렇게 Person class에서 선언한 것 과 동일한 결과가 나옴
     }
+    get name() {
+        // 함수이므로 무언가 로직을 만들 수 있음
+        // console.log("get");
+        return this._name + "잘생긴";
+    }
+    set name(name1) {
+        console.log("set");
+        this._name = name1;
+    }
 }
 const p1 = new Person("Sim", 24);
 // const p2: Person = new Person();
-console.log(p1);
+console.log(p1.name); // p1에서 꺼내오는 것이기 때문에 get을 하는 함수 getter
+p1.name = "ChaeWoon"; // name이라는 프로퍼티의 데이터 값을 바꾸는 것이기 때문 set을 하는 함수 setter
+console.log(p1.name);
 // console.log(p1.age);
 // console.log(p2);
--- class/example.ts
+++ class/example.ts
@@ -6,7 +6,7 @@ class Person {
   //   this.name = name;
   // }
   // constructor 에는 async 붙힐 수 없음
-  public constructor(public name: string, private age: number) {
+  public constructor(private _name: string, private age: number) {
     // if (age === undefined) { // 두개의 class 생성을 하고싶으면 ex p1, p2 조건부로 제한
     //   this.age = 20;
     // } else {
@@ -14,11 +14,26 @@ class Person {
     // }
     // 원래는 this.name = name , this.age = age 이렇게 작성을 해야하지만 코드가 길어지므로 지워버리고 constructor 매개변수 앞에 접근제어자를 붙여주면 name: string; age: number; 이렇게 Person class에서 선언한 것 과 동일한 결과가 나옴
   }
+
+  get name() {
+    // 함수이므로 무언가 로직을 만들 수 있음
+    // console.log("get");
+    return this._name + "잘생긴";
+  }
+
+  set name(name1: string) {
+    console.log("set");
+    this._name = name1;
+  }
 }
 
 const p1: Person = new Person("Sim", 24);
 // const p2: Person = new Person();
 
-console.log(p1);
+console.log(p1.name); // p1에서 꺼내오는 것이기 때문에 get을 하는 함수 getter
+p1.name = "ChaeWoon"; // name이라는 프로퍼티의 데이터 값을 바꾸는 것이기 때문 set을 하는 함수 setter
+console.log(p1.name);
+// 즉 get, set 함수를 이용해 외부에서 데이터를 바꾸는 것의 제한을 걸 수 있음.
+
 // console.log(p1.age);
 // console.log(p2);
