ad52f6f15b0056fca14219caa266052411395ed5
Sim <0doogadooga@gmail.com>
2022-12-04 21:37:58 +0900

    initialization in constructor parameters

--- class/example.js
+++ class/example.js
@@ -1,21 +1,24 @@
 "use strict";
 class Person {
+    // 암묵적으로 private로 선언하면 _를 붙임 ex) private _age!:number
     // constructor(name: string) {
     //   //생성자
     //   this.name = name;
     // }
-    constructor(age) {
-        this.name = "Sim";
-        if (age === undefined) {
-            this.age = 20;
-        }
-        else {
-            this.age = age;
-        }
+    // constructor 에는 async 붙힐 수 없음
+    constructor(name, age) {
+        this.name = name;
+        this.age = age;
+        // if (age === undefined) { // 두개의 class 생성을 하고싶으면 ex p1, p2 조건부로 제한
+        //   this.age = 20;
+        // } else {
+        //   this.age = age;
+        // }
+        // 원래는 this.name = name , this.age = age 이렇게 작성을 해야하지만 코드가 길어지므로 지워버리고 constructor 매개변수 앞에 접근제어자를 붙여주면 name: string; age: number; 이렇게 Person class에서 선언한 것 과 동일한 결과가 나옴
     }
 }
-const p1 = new Person(24);
-const p2 = new Person();
+const p1 = new Person("Sim", 24);
+// const p2: Person = new Person();
 console.log(p1);
-console.log(p1.age);
-console.log(p2);
+// console.log(p1.age);
+// console.log(p2);
--- class/example.ts
+++ class/example.ts
@@ -1,20 +1,19 @@
 class Person {
-  public name: string = "Sim";
-  private _age!: number; // 암묵적으로 private로 선언하면 _를 붙임
+  // 암묵적으로 private로 선언하면 _를 붙임 ex) private _age!:number
+
   // constructor(name: string) {
   //   //생성자
   //   this.name = name;
   // }
   // constructor 에는 async 붙힐 수 없음
-  public constructor(name: string, age?: number) {
+  public constructor(public name: string, private age: number) {
     // if (age === undefined) { // 두개의 class 생성을 하고싶으면 ex p1, p2 조건부로 제한
     //   this.age = 20;
     // } else {
     //   this.age = age;
     // }
+    // 원래는 this.name = name , this.age = age 이렇게 작성을 해야하지만 코드가 길어지므로 지워버리고 constructor 매개변수 앞에 접근제어자를 붙여주면 name: string; age: number; 이렇게 Person class에서 선언한 것 과 동일한 결과가 나옴
   }
-
-  public async init() {}
 }
 
 const p1: Person = new Person("Sim", 24);
